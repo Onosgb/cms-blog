be3be8e47112a77a557525eb5ad7ddf28065ede9
Onosgb <onosgb@gmail.com>
2021-12-24 02:25:03 +0100

    splited the the code into reusable code by seperating the footer, header, navigation, and sidebar into different page

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,42 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#post_dropdown"><i class="fa fa-fw fa-envelope"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="post_dropdown" class="collapse">
+                            <li>
+                                <a href="#">View All Posts</a>
+                            </li>
+                            <li>
+                                <a href="#">Add Post</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-list"></i> Categories</a>
+                    </li>
+                    <li class="active">
+                        <a href="blank-page.html"><i class="fa fa-fw fa-envelope"></i> Comments</a>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#users"><i class="fa fa-fw fa-users"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="users" class="collapse">
+                            <li>
+                                <a href="#">Dropdown Item</a>
+                            </li>
+                            <li>
+                                <a href="#">Dropdown Item</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="blank-page.html"><i class="fa fa-fw fa-user"></i> Profile</a>
+                    </li>
+                   
+                </ul>
+            </div>
